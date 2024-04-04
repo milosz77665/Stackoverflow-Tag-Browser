0d2fe3e95a229a62c1287055a115d1e72f20dc35
Miłosz Marchewka <milosz77665@gmail.com>
2024-04-04 17:35:37 +0200

    add defer and Suspense to handle loading state

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,31 @@
 import DataTable from "../components/DataTable";
-import { useLoaderData } from "react-router-dom";
+import { Await, defer, useLoaderData } from "react-router-dom";
 import Tag from "../models/Tag";
 import TagsData from "../models/TagsData";
+import { Suspense } from "react";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 const Home = () => {
-  const tagsData = useLoaderData() as TagsData;
+  const { tagsData } = useLoaderData() as { tagsData: TagsData };
 
   return (
-    <DataTable<Tag>
-      data={tagsData ? tagsData.items : []}
-      keyNames={["name", "count"]}
-      columnNames={["Tag name", "Count"]}
-    />
+    <Suspense fallback={<LoadingSpinner size={80} />}>
+      <Await resolve={tagsData}>
+        {(loadedTagsData: TagsData) => (
+          <DataTable<Tag>
+            data={loadedTagsData ? loadedTagsData.items : []}
+            keyNames={["name", "count"]}
+            columnNames={["Tag name", "Count"]}
+          />
+        )}
+      </Await>
+    </Suspense>
   );
 };
 
 export default Home;
 
-export const tagsDataLoader = async () => {
+const fetchTagsData = async () => {
   const response = await fetch(
     "https://api.stackexchange.com/2.3/tags?order=desc&sort=popular&site=stackoverflow&key=dD4kEfcU7eiZkzj9eOBFsQ(("
   );
@@ -27,4 +35,10 @@ export const tagsDataLoader = async () => {
     const data: TagsData = await response.json();
     return data;
   }
+};
+
+export const tagsDataLoader = async () => {
+  return defer({
+    tagsData: fetchTagsData(),
+  });
 };
